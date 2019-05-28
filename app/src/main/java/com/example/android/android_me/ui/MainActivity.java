e3b7bde3c2e12991478208738e8fe2b1f3f3addc
GretaGri <greta.klimaite@gmail.com>
2019-05-28 23:13:03 +0300

    Completed lesson 2 exercise 05 - interface communications between fragment and activity.

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -18,12 +18,13 @@ package com.example.android.android_me.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.example.android.android_me.R;
 
 // This activity is responsible for displaying the master list of all images
-// TODO (4) Implement the MasterListFragment callback, OnImageClickListener
-public class MainActivity extends AppCompatActivity {
+// Done (4) Implement the MasterListFragment callback, OnImageClickListener
+public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener {
 
 
     @Override
@@ -32,7 +33,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
-
-    // TODO (5) Define the behavior for onImageSelected; create a Toast that displays the position clicked
-
+    // Done (5) Define the behavior for onImageSelected; create a Toast that displays the position clicked
+    @Override
+    public void onImageSelected(int position) {
+        Toast.makeText(this,"Position of clicked item is: " + position, Toast.LENGTH_SHORT).show();
+    }
 }
